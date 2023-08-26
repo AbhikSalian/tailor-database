63d86f674148a86b91f110830317eeeb3724d2b2
Abhik Salian <21e03.abhik@sjec.ac.in>
2023-08-26 21:51:34 +0530

    many changes

--- sign_up.php
+++ sign_up.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartStitch-Sign Up</title>
-    <!-- <link rel="stylesheet" href="SignUp.css"> -->
+    <link rel="stylesheet" href="SignUp.css">
 </head>
 <body>
     <form action="sign_up.php" method="post">
