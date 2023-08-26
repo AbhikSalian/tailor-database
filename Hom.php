73b770d282c97b7c5e91330e6845a9bdf1655599
AkashAcharya03 <akashacharya2003@gmail.com>
2023-08-26 21:01:33 +0530

    1

--- Hom.php
+++ Hom.php
@@ -1,37 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width= , initial-scale=1.0">
     <title>SmartStitch-Home</title>
     <link rel="stylesheet" href="Hom.css">
 </head>
+
 <body>
     <header>
         <nav class="navbar">
-            <div class="navdiv">          
-            <div class="logo"><a href="Home.html"><img src="SmartStitchLogo.png" alt="SmartStitch" height="40px"></a></div>
-            <ul>
-            <li class="home"><a href="#">Home</a></li>
-            <li><a href="measurement.php">Men</a></li>
-            <li><a href="#">Women</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="client_details.php">Profile</a></li>
-            <a href="logout.php"><button class="signout">Sign Out</button></a>
-        </ul>
-    </div>
+            <div class="navdiv">
+                <div class="logo"><a href="Home.html"><img src="SmartStitchLogo.png" alt="SmartStitch" height="40px"></a></div>
+                <ul class="list">
+                    <li class="home"><a href="#">Home</a></li>
+                    <li><a href="measurement.php">Men</a></li>
+                    <li><a href="#">Women</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="client_details.php">Profile</a></li>
+                    <a href="logout.php"><button class="signout">Sign Out</button></a>
+                </ul>
+            </div>
         </nav>
     </header>
-    
+
 
     <main>
-
+      <div>
+      </div>
     </main>
 
     <footer>
-        
+      <div class="footer">
+        <div class="contact">
+          <ul>
+            <h3>Contact</h3>
+
+            <li><i class="fa-solid fa-phone" style="color: #fff6e5;"></i> 7760447896</li>
+            <li><a href="mailto: akashacharya2003@gmail.com"><i class="fa-solid fa-envelope" style="color: #fff6e5;"></i> akashacharya2003@gmail.com</li></a>
+          </ul>
+        </div>
+        <div class="business">
+          <ul>
+            <h3>Business Hours</h3>
+            <li>Monday - Friday : 09:00 AM - 06:00 PM</li>
+            <li>Saturday : 09:00 AM - 04:00 PM</li>
+            <li>Sunday : Closed</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <h5 class="copyright">
+        @copyright 2023 SmartStitch - All Rigths Reserved
+      </h5>
     </footer>
 
 </body>
+
 </html>
