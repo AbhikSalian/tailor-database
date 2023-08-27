5d1e0c41bf746125058b4df36b8b9812c4f32b43
AkashAcharya03 <akashacharya2003@gmail.com>
2023-08-27 15:36:57 +0530

    2

---
+++ pants.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartSticth-Pants</title>
+    <link rel="stylesheet" href="pants.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="navdiv">
+                <div class="logo"><a href="Hom.php"><img src="SmartStitchLogo.png" alt="SmartStitch" height="40px"></a></div>
+                <ul class="list">
+                    <li><a href="Hom.php">Home</a></li>
+                    <li ><a href="shirts.php">Shirts</a></li>
+                    <li class="home"><a href="pants.php">Pants</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="client_details.php">Profile</a></li>
+                    <a href="logout.php"><button class="signout">Sign Out</button></a>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+
+    <main>
+      <div class="fabric">
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric1.jpg" alt="" height="250px">           
+              <p>Colour Checks Pants Fabric Black Honey Day</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>545.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+          
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric2.jpg" alt="" height="250px">           
+              <p>Cotton Colour Checked Grey Suiting Fabric Fun</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>655.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric3.jpg" alt="" height="250px">           
+              <p>Colour Off White Checks Pants Fabric Honey Day</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>545.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric4.jpg" alt="" height="250px">           
+              <p>Cotton Colour Checked Pant Fabric Sandal Chronicle</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>625.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric5.jpg" alt="" height="250px">           
+              <p>Cotton Colour Plain Pants Fabric Navy Style Craft</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>660.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric6.jpg" alt="" height="250px">           
+              <p>Cotton Colour Checked Pants Fabric Grayish Blue</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>670.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric7.jpg" alt="" height="250px">           
+              <p>Wrinkle Free Cotton White Plain Pants Fabric Romantic</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>695.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          <div class="shirt">
+              <img class="shirtimg" src="Images/pantsFabric8.jpg" alt="" height="250px">           
+              <p>Cotton Colour Checked Pant Fabric Navy ICLE Stretch</p>
+              <p><i class="fa-solid fa-indian-rupee-sign" style="color: #fff6e5;"></i> <Rs class="money"></Rs>740.00</p>
+              <div class="placeorder">
+                <a href="#"><button><i class="fa-solid fa-cart-shopping" style="color: #062f3d;"></i> Place Order</button></a>
+              </div>
+  
+          </div>
+          
+
+      </div>
+    </main>
+
+    <footer>
+      <div class="footer">
+        <img src="logo bg.png" alt="" height="120px">
+        <div class="contact">
+          <ul>
+            <h4>CONTACT</h4>
+
+            <li><i class="fa-solid fa-phone" style="color: #42707f;"></i> 7760447896</li>
+            <li><a href="mailto: akashacharya2003@gmail.com"><i class="fa-solid fa-envelope" style="color: #42707f;"></i> akashacharya2003@gmail.com</li></a>
+          </ul>
+        </div>
+        <div class="business">
+          <ul>
+            <h4>BUSINESS HOURS</h4>
+            <li>Monday - Friday : 09:00 AM - 06:00 PM</li>
+            <li>Saturday : 09:00 AM - 04:00 PM</li>
+            <li>Sunday : Closed</li>
+          </ul>
+        </div>
+      </div>
+      <!-- <hr /> -->
+      <h5 class="copyright">
+        @copyright 2023 SmartStitch - All Rigths Reserved
+      </h5>
+    </footer>
+
+</body>
+</html>
