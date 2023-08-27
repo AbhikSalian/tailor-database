504ca63865669ccb8750548b382641205d6df31a
AkashAcharya03 <akashacharya2003@gmail.com>
2023-08-27 16:21:50 +0530

    2

--- sign_up.php
+++ sign_up.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartStitch-Sign Up</title>
     <link rel="stylesheet" href="SignUp.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
 </head>
 <body>
     <form onSubmit="return checkPassword(this)" action="sign_up.php" method="post">
