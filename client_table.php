54c5a07269cb73e941d659e77627be5c5c02e5f1
AkashAcharya03 <akashacharya2003@gmail.com>
2023-09-03 08:37:17 +0530

    Updated many

--- client_table.php
+++ client_table.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SmartStitch-tailor-ClienttDetails</title>
+    <title>SmartStitch-tailor-ClientDetails</title>
     <link rel="stylesheet" href="tailorDatabase.css">
 </head>
 <body>
--- tailor_interface.php
+++ tailor_interface.php
@@ -12,7 +12,7 @@
     <header>
         <nav class="navbar">
             <div class="navdiv">
-                <div class="logo"><a href="Home.html"><img src="SmartStitchLogo.png" alt="SmartStitch" height="40px"></a></div>
+                <div class="logo"><a href="tailor_interface.php"><img src="SmartStitchLogo.png" alt="SmartStitch" height="40px"></a></div>
             </div>
             <p class="home">Tailor Interface</p>
             <span><a href="welcomePage.php"><button>Logout</button></a></span>
